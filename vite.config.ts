1d4262f64e470e6871cf172fa5fc7d91b1364813
Agoria <dylan@agoria.io>
2026-05-16 02:22:14 +0200

    Remove lovable-tagger from build pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
-import vitePrerender from "vite-plugin-prerender";
-
-const PrerenderRoutes = [
-  "/",
-  "/studio",
-  "/portfolio",
-  "/contact",
-  "/portfolio/eclipsia",
-  "/portfolio/altarys",
-  "/portfolio/prophecia",
-  "/services/web",
-  "/services/mobile",
-  "/services/backoffice",
-  "/services/360",
-];
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-    mode === "production" &&
-      vitePrerender({
-        staticDir: path.join(__dirname, "dist"),
-        routes: PrerenderRoutes,
-      }),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
